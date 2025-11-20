67c078ae7a8835869333e7b63081b9155dc9eb0c
mdshadab41 <shadabjhu00447@gmail.com>
2025-11-20 07:41:15 +0530

    Add: Array Concatenation solution

---
+++ 01-Array-and-Hashing/001-array-concatenation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+
+*/
+
+vector<int> getConcatenation(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans(2 * n);
+
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> nums = {1, 2, 1};
+    vector<int> ans = getConcatenation(nums);
+
+    for (int x : ans)
+    {
+        cout << x << " ";
+    }
+}
